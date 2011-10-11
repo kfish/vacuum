e2e4e5559f43c408bee91d866512b20e62fb41d7
Conrad Parker <conrad@metadecks.org>
2011-10-11 08:36:11 +0800

    Release 0.0.7

--- src/GHC/Vacuum.hs
+++ src/GHC/Vacuum.hs
@@ -43,11 +43,14 @@ module GHC.Vacuum (
   ,ShowHNode(..)
   ,showHNodes
   ,ppHs,ppDot
+  ,Draw(..),G(..)
+  ,draw,printDraw,split
   ,Closure(..)
   ,InfoTab(..)
   ,getClosure
   ,nodePkg,nodeMod
   ,nodeName,itabName
+  ,getInfoPtr
 ) where
 import GHC.Vacuum.Dot as Dot
 import GHC.Vacuum.ClosureType
@@ -85,6 +88,10 @@ dump a = execH (dumpH a)
 
 dumpTo :: Int -> a -> IO (IntMap HNode)
 dumpTo n a = execH (dumpToH n a)
+
+
+
+
 
 -----------------------------------------------------------------------------
 
@@ -153,6 +160,45 @@ instance Show HValue where show _ = "(HValue)"
 
 ------------------------------------------------
 
+-- | To assist in \"rendering\"
+--  the graph to some source.
+data Draw e v m a = Draw
+  {mkV   :: Int -> a -> m v
+  ,mkE   :: v -> v -> m e
+  ,succs :: a -> [Int]}
+
+newtype G e v = G {unG :: IntMap (v, IntMap e)}
+  deriving(Eq,Ord,Read,Show)
+
+draw :: (Monad m) => Draw e v m a -> IntMap a -> m (G e v)
+draw (Draw mkV mkE succs) g = do
+  vs <- IM.fromList `liftM` forM (IM.toList g)
+          (\(i,a) -> do v <- mkV i a
+                        return (i,(v,succs a)))
+  (G . IM.fromList) `liftM` forM (IM.toList vs)
+    (\(i,(v,ps)) -> do let us = fmap (vs IM.!) ps
+                       es <- IM.fromList `liftM` forM ps
+                               (\p -> do e <- mkE v (fst (vs IM.! p))
+                                         return (p,e))
+                       return (i,(v,es)))
+
+-- | An example @Draw@
+printDraw :: Draw (Int,Int) Int IO HNode
+printDraw = Draw
+  {mkV   = \i _ -> print i >> return i
+  ,mkE   = \u v -> print (u,v) >> return (u,v)
+  ,succs = nodePtrs}
+
+-- | Build a map to @(preds,succs)@
+split :: (a -> [Int]) -> IntMap a -> IntMap ([Int],[Int])
+split f = flip IM.foldWithKey mempty (\i a m ->
+            let ps = f a
+            in foldl' (\m p -> IM.insertWith mappend p ([i],[]) m)
+                      (IM.insertWith mappend i ([],ps) m)
+                      ps)
+
+------------------------------------------------
+
 emptyHNode :: ClosureType -> HNode
 emptyHNode ct = HNode
   {nodePtrs   = []
@@ -177,6 +223,14 @@ itabName i@(ConInfo{}) = (itabPkg i, itabMod i, itabCon i)
 itabName  _            = ([], [], [])
 
 ------------------------------------------------
+
+getInfoPtr :: a -> Ptr StgInfoTable
+getInfoPtr a = let b = a `seq` Box a
+                in b `seq` case unpackClosure# a of
+                            (# iptr,_,_ #)
+                              | ghciTablesNextToCode -> Ptr iptr
+                              | otherwise -> Ptr iptr `plusPtr`
+                                              negate wORD_SIZE
 
 -- | This is in part borrowed from @RtClosureInspect.getClosureData@.
 getClosure :: a -> IO Closure
